98574b0afc3ccd470d7dd1f6615916d04d54e936
vayne <miguel.osorio19@yahoo.com>
2022-04-01 15:30:38 -0500

    added login and addUser mutations via gql template functionality

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,27 @@
 import { gql } from '@apollo/client';
 
+// import the gql tagged template literal functionality to create a GraphQL mutation called login
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
+`;
+
+// import the gql tagged template literal functionality to create a GraphQL mutation called addUser
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
