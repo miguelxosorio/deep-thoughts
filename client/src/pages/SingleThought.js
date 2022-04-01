abe2528e87b1282230df5a108742fab567ef6e0d
vayne <miguel.osorio19@yahoo.com>
2022-04-01 12:09:50 -0500

    added single thought gql query and imported on SingleThought.js

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,21 +1,40 @@
 import React from 'react';
 // useParams
 import { useParams } from 'react-router-dom';
+// import useQuery() 
+import { useQuery } from '@apollo/client';
+// import QUERY_THOUGHT
+import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
   console.log(thoughtId)
+
+  // variables loading and data are destructured from the useQuery Hook
+  //  The loading variable is then used to briefly show a loading <div> element, and the data variable is used to populate a thought object
+  // The useQuery Hook was given a second argument in the form of an object. This is how you can pass variables to queries that need them
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    // The id property on the variables object will become the $id parameter in the GraphQL query
+    variables: { id: thoughtId }
+  });
+
+  const thought = data?.thought || {};
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}
           </span>{' '}
           thought on createdAt
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
     </div>
